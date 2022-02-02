f6676c0992f28efe5b91739e746bd636566aaf69
faizel1 <faizelakmel@gmail.com>
2022-02-02 12:15:02 +0300

    session storage changed to redis

--- application/config/config.php
+++ application/config/config.php
@@ -378,13 +378,13 @@ $config["encryption_key"] = "";
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config["sess_driver"] = "database";
-$config["sess_cookie_name"] = "ci_sessions";
-$config["sess_expiration"] = 7200;
-$config["sess_save_path"] = "easy_commerce.ci_sessions";
-$config["sess_match_ip"] = true;
-$config["sess_time_to_update"] = 300;
-$config["sess_regenerate_destroy"] = true;
+$config['sess_driver'] = 'redis';
+$config['sess_save_path'] = 'tcp://localhost:6379';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
